ab047ac1047f3d04db0bd1f2e5832820343ad868
NicoTheDuck <nicolas.coral1@gmail.com>
2014-08-16 10:58:32 +0200

    Première contribution

--- src/model/AbstractModel.java
+++ src/model/AbstractModel.java
@@ -5,36 +5,31 @@ import java.util.ArrayList;
 import observer.Observable;
 import observer.Observer;
 
+public abstract class AbstractModel implements Observable {
 
-public abstract class AbstractModel implements Observable{
+	protected ArrayList<Observer> listObserver = new ArrayList<>();
+	protected ArrayList<Player> listPlayer = new ArrayList<>();
 
-  protected ArrayList<Observer> listObserver = new ArrayList<Observer>();
-  protected ArrayList<Player> listPlayer = new ArrayList<Player>();
-  
-  public abstract void updatePV();
-  
-  
-  public void addPlayer(Player p){
-	  listPlayer.add(p);
-  }
-  
-  public void removePlayer(Player p){
-	  if(listPlayer.contains(p)){
-		  listPlayer.remove(listPlayer.indexOf(p));
-	  }
-	  
-  }
-  
-  //Implémentation du pattern observer
-  public void addObserver(Observer obs) {
-    this.listObserver.add(obs);
-  }
+	public abstract void updatePV();
 
-  public void notifyObserver(int i) {
-  
-  }
+	public void addPlayer(Player p) {
+		listPlayer.add(p);
+	}
 
-  public void removeObserver() {
-    listObserver = new ArrayList<Observer>();
-  }  
+	public void removePlayer(Player p) {
+		listPlayer.remove(p);
+	}
+
+	// Implémentation du pattern observer
+	public void addObserver(Observer obs) {
+		this.listObserver.add(obs);
+	}
+
+	public void notifyObserver(int i) {
+
+	}
+
+	public void removeObserver() {
+		listObserver = new ArrayList<>();
+	}
 }
