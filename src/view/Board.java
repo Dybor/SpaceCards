673ffdf3662e108cc8e9bd93778083b9d43b9850
Dybor <benja-myn@hotmail.fr>
2014-08-25 19:10:51 +0200

    Mise à jour IHM avec un Panel central. regroupement de la main du joueur avec les plateaux de joueurs

--- src/view/Board.java
+++ src/view/Board.java
@@ -1,6 +1,7 @@
 package view;
 
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
@@ -9,6 +10,8 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
@@ -28,30 +31,28 @@ import javax.swing.JTextArea;
 
 
 
+
+
+
 import model.drawable.IDrawableBoard;
 import model.drawable.IDrawableHand;
 import controler.GameControler;
 import controler.INotifyView;
 import controler.Observable;
 
-public class Board extends JFrame implements ActionListener, MouseListener, MouseMotionListener, ItemListener, INotifyView{
+public class Board extends JFrame implements ActionListener, MouseListener, MouseMotionListener, ItemListener, INotifyView, ComponentListener{
 
 
 //L'instance de notre objet contrôleur
 private GameControler controler;
 private Observable game;
 
-private JPanel board;
+private GamePanel board;
 
 
 private ZoomCardPanel zoomCard;
 private PlayerStatus pStatus;
 
-private JTextArea playerEvent;
-
-private Hand hand;
-private BoardCard playerBoard;
-private ArrayList<BoardCard> otherPlayerBoard;
 private GameRound gameRound;
 
 // Menu
@@ -100,9 +101,7 @@ private void initComposant(){
 	initBoard();			//Initialisation du board central
 	initZoomCard();			//Initialisation de l'espace pour la carte zoomée
 	initMenu();				//Initialisation de la barre de menu
-	initHands();			//Initialisation de la main du joueur
 	initGameRound();		//Initialisation des tours de jeu 
-	initPlayerEvent();		//Initialisation des évenements de jeu
 	initPlayerStatus();		//Initialisation du panneau de status du joueur
 	
 	initTestModel();
@@ -113,18 +112,7 @@ private void initComposant(){
 }   
 
 private void initBoard(){
-	board = new JPanel();
-	playerBoard = new BoardCard(6,2,0,10,0,0,Board.WIDTH_CARD_BOARD,Board.HEIGHT_CARD_BOARD,Board.POSITION_X_BOARD,Board.POSITION_Y_BOARD);
-	playerBoard.addMouseMotionListener(this);
-	playerBoard.addMouseListener(this);
-//	board.setPreferredSize(new Dimension(10*Board.WIDTH_CARD_HAND, playerBoard.getHeightPanel()));
-	
-	
-
-	
-	
-	
-	
+	board = new GamePanel(1,this.getWidth(),this.getHeight());
 }
 
 
@@ -146,11 +134,6 @@ private void initMenu(){
 	menuBar.add(menu_Option);
 }
 
-private void initHands(){
-	hand = new Hand();
-	hand.addMouseListener(this);
-	hand.addMouseMotionListener(this);
-}
 
 private void initZoomCard(){
 	zoomCard = new ZoomCardPanel();
@@ -162,65 +145,19 @@ private void initGameRound(){
 	
 }
 
-private void initPlayerEvent(){
-	playerEvent = new JTextArea();
-	playerEvent.setFocusable(false);
-}
-
 private void initPlayerStatus(){
 	pStatus = new PlayerStatus(7);
 }
 
 
 private void initFrame(){
-	this.setLayout(new GridBagLayout());
-	GridBagConstraints gbc = new GridBagConstraints();
-	
-	gbc.gridx = 0 ; gbc.gridy = 0; // la grille commence en (0,0)
-	gbc.fill = GridBagConstraints.VERTICAL;
-	gbc.fill = GridBagConstraints.HORIZONTAL;
-	gbc.gridwidth = 1;
-	gbc.gridheight = 2;
-	gbc.anchor = GridBagConstraints.CENTER;
-	this.add(board,gbc);
-	
-	gbc.gridx = 1 ; gbc.gridy = 0;
-	gbc.fill = GridBagConstraints.NONE;
-	gbc.fill = GridBagConstraints.NONE;
-	gbc.gridwidth = GridBagConstraints.RELATIVE;
-	gbc.gridheight = 2;
-	gbc.anchor = GridBagConstraints.LINE_END;
-	this.add(gameRound,gbc);
-	
-	gbc.gridx = 2 ; gbc.gridy = 1;
-	gbc.gridwidth = GridBagConstraints.REMAINDER;
-	gbc.gridheight = GridBagConstraints.RELATIVE;
-	gbc.anchor = GridBagConstraints.LINE_END;
-	this.add(zoomCard,gbc);
-	
-	gbc.gridx = 0 ; gbc.gridy = 2;
-	gbc.gridwidth = GridBagConstraints.REMAINDER;
-	gbc.gridheight = GridBagConstraints.RELATIVE;
-	gbc.fill = GridBagConstraints.HORIZONTAL;
-	gbc.anchor = GridBagConstraints.PAGE_END;
-	this.add(playerEvent,gbc);
-	
-	gbc.gridx = 0; gbc.gridy = 3;
-	gbc.gridheight = GridBagConstraints.REMAINDER;
-	gbc.gridwidth = GridBagConstraints.RELATIVE;
-	gbc.fill = GridBagConstraints.HORIZONTAL;
-	gbc.anchor = GridBagConstraints.LAST_LINE_START;
-	this.add(hand,gbc);
-	
-	gbc.gridx = 2; gbc.gridy = 3;
-	gbc.gridheight = GridBagConstraints.REMAINDER;
-	gbc.gridwidth = GridBagConstraints.REMAINDER;
-	gbc.fill = GridBagConstraints.HORIZONTAL;
-	gbc.anchor = GridBagConstraints.LAST_LINE_START;
-	this.add(pStatus,gbc);
+	this.setLayout(new BorderLayout());
+	this.add(board, BorderLayout.CENTER);
+	this.add(gameRound, BorderLayout.WEST);
+	this.add(zoomCard, BorderLayout.EAST);
 
 	this.setJMenuBar(menuBar);
-	
+	this.addComponentListener(this);
 	
 }
 
@@ -277,33 +214,7 @@ public void actionPerformed(ActionEvent e) {
 		 
 		 sendInformationMessage("Tour1");
 		 
-		 
-			
-			
-			if(game!=null){
-				//int nbPlayer = game.getPlayers().size();
-				int nbPlayer = 4;
-				
-				board.setLayout(new GridLayout(2,nbPlayer-1));
-				
-				if(nbPlayer >1)
-				{
-					otherPlayerBoard = new ArrayList<>();
-					for(int i = 0;i<nbPlayer-1;i++){
-						BoardCard otherPlayer = new BoardCard(6, 2, 0, 0, 0, 0, Board.WIDTH_CARD_BOARD,Board.HEIGHT_CARD_BOARD, 
-								Board.POSITION_X_BOARD, Board.POSITION_Y_BOARD);
-						otherPlayer.addMouseMotionListener(this);
-						otherPlayer.addMouseListener(this);
-						otherPlayerBoard.add(otherPlayer);
-						
-						board.add(otherPlayer);
-					}
-				
-				}
-				
-				board.add(playerBoard);
-			}
-			
+		 refreshSizeGamePanel();
 		 
 	 } else if(e.getSource()==item_quit){
 		System.exit(0); 
@@ -312,16 +223,26 @@ public void actionPerformed(ActionEvent e) {
 	
 }
 
+public void refreshSizeGamePanel(){
+	board.setWidth_Screen(board.getWidth());
+	board.setHeight_Screen(board.getHeight());
+	board.repaint();
+}
+
 public void updatePoolPV(int pv) {
 	
 }
 
 public void updateHand(IDrawableHand hand){
-	this.hand.updateCards(hand);
+	this.board.updateCards(hand);
 }
 
 public void updateBoard(IDrawableBoard board){
-	playerBoard.updateBoard(board);
+	this.board.updatePlayerBoard(board);
+}
+
+public void updateOtherBoard(ArrayList<IDrawableBoard> list){
+	this.board.updateOtherPlayerBoard(list);
 }
 
 public void updateGameRound(ArrayList<Boolean> bool) {
@@ -330,7 +251,6 @@ public void updateGameRound(ArrayList<Boolean> bool) {
 }
 
 public void sendInformationMessage(String str) {
-	playerEvent.setText(str);
 	
 }
 
@@ -340,78 +260,59 @@ public void sendInformationMessage(String str) {
 
 @Override
 public void mouseClicked(MouseEvent e) {
-	if(e.getComponent().equals(hand)){
-		hand.mouseClicked(e);
-	} else if(e.getComponent().equals(playerBoard)){
-		playerBoard.mouseClicked(e);
-	}
+//	if(e.getComponent().equals(board)){
+//		board.mouseClicked(e);
+//	} 
 	
 }
 
 @Override
 public void mouseEntered(MouseEvent e) {
-	if(e.getComponent().equals(hand)){
-		hand.mouseEntered(e);
-	} else if(e.getComponent().equals(playerBoard)){
-		playerBoard.mouseEntered(e);
-	}
+//	if(e.getComponent().equals(board)){
+//		board.mouseEntered(e);
+//	} 
 }
 
 @Override
 public void mouseExited(MouseEvent e) {
-	if(e.getComponent().equals(hand)){
-		hand.mouseExited(e);
-		zoomCard.setCard(null);
-	} else if(e.getComponent().equals(playerBoard)){
-		playerBoard.mouseExited(e);
-	}
+//	if(e.getComponent().equals(board)){
+//		board.mouseExited(e);
+//		zoomCard.setCard(null);
+//	}
 }
 
 @Override
 public void mousePressed(MouseEvent e) {
-	if(e.getComponent().equals(hand)){
-		hand.mousePressed(e);
-	} else if(e.getComponent().equals(playerBoard)){
-		playerBoard.mousePressed(e);
-	}
+//	if(e.getComponent().equals(board)){
+//		board.mousePressed(e);
+//	} 
 }
 
 @Override
 public void mouseReleased(MouseEvent e) {
-	if(e.getComponent().equals(hand)){
-		hand.mouseReleased(e);
-	} else if(e.getComponent().equals(playerBoard)){
-		playerBoard.mouseReleased(e);
-	}
+//	if(e.getComponent().equals(board)){
+//		board.mouseReleased(e);
+//	} 
 }
 
 @Override
 public void mouseDragged(MouseEvent e) {
-	if(e.getComponent().equals(hand)){
-		hand.mouseDragged(e);
-	} else if(e.getComponent().equals(playerBoard)){
-		playerBoard.mouseDragged(e);
-	}
+//	if(e.getComponent().equals(board)){
+//		board.mouseDragged(e);
+//	} 
 }
 
 @Override
 public void mouseMoved(MouseEvent e) {
-	if(e.getComponent().equals(hand)){
-		hand.mouseMoved(e);
-		if(hand.getFocusedCard()!=null){
-			zoomCard.setCard(new GraphicCard(hand.getFocusedCard().getImage(),0,0,Board.WIDTH_CARD_ZOOM, Board.HEIGHT_CARD_ZOOM));
-		} else {
-			zoomCard.setCard(null);
-		}
-	
-	} else if(e.getComponent().equals(playerBoard)){
-		playerBoard.mouseMoved(e);
-		if(playerBoard.getFocusedCard()!=null){
-			zoomCard.setCard(new GraphicCard(playerBoard.getFocusedCard().getImage(),0,0,Board.WIDTH_CARD_ZOOM, Board.HEIGHT_CARD_ZOOM));
-		} else {
-			zoomCard.setCard(null);
-		}
-	}
+//	if(e.getComponent().equals(board)){
+//		board.mouseMoved(e);
+//		if(board.getFocusedCard()!=null){
+//			zoomCard.setCard(new GraphicCard(board.getFocusedCard().getImage(),0,0,Board.WIDTH_CARD_ZOOM, Board.HEIGHT_CARD_ZOOM));
+//		} else {
+//			zoomCard.setCard(null);
+//		}
+//	
+//	} 
 }
 
 @Override
@@ -419,6 +320,29 @@ public void itemStateChanged(ItemEvent e) {
 	if(e.getSource()==item_fullScreen){
 		fullScreen(((JCheckBoxMenuItem)e.getSource()).getState());
 	}
+	
+}
+
+@Override
+public void componentHidden(ComponentEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void componentMoved(ComponentEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void componentResized(ComponentEvent e) {
+	refreshSizeGamePanel();
+}
+
+@Override
+public void componentShown(ComponentEvent e) {
+	// TODO Auto-generated method stub
 	
 }
 
---
+++ src/view/GamePanel.java
@@ -0,0 +1,434 @@
+package view;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.JPanel;
+
+import model.Card;
+import model.drawable.IDrawableBoard;
+import model.drawable.IDrawableCard;
+import model.drawable.IDrawableHand;
+
+public class GamePanel extends JPanel implements MouseListener, MouseMotionListener{
+
+	private ArrayList<GraphicCard> playerBoard = new ArrayList<GraphicCard>();
+	private ArrayList<GraphicCard> hand = new ArrayList<GraphicCard>();
+	private ArrayList<ArrayList<GraphicCard>> otherPlayerBoard = new ArrayList<>();
+	private GraphicCard zoomCard ;
+	
+	private int flagHandCard = -1;
+	private int flagPlayerBoardCard = -1;
+	private ArrayList<Integer> flagOtherPlayerBoardCard = new ArrayList<>();
+	private boolean focusACard;
+	
+	private int nbPlayer = 1;
+	
+	
+	// Player Board
+	
+	private int position_X_P = 0;
+	private int position_Y_P = 0;
+	private int nbCardbyRow_P = 6;
+	private int nbRow_P = 2;
+	private int scale_width_P = Board.WIDTH_CARD_BOARD;
+	private int scale_height_P = Board.HEIGHT_CARD_BOARD;
+	private int marge_ext_x_P = 20;
+	private int marge_int_x_P = 10;
+	private int marge_ext_y_P = 10;
+	private int marge_int_y_P = 80;
+	
+	// Other Player Board
+	
+	private int position_X_OP = 0;
+	private int position_Y_OP = 0;
+	private int nbCardbyRow_OP = 6;
+	private int nbRow_OP = 2;
+	private int scale_width_OP = Board.WIDTH_CARD_BOARD;
+	private int scale_height_OP = Board.HEIGHT_CARD_BOARD;
+	private int marge_ext_x_OP = 20;
+	private int marge_int_x_OP = 10;
+	private int marge_ext_y_OP = 10;
+	private int marge_int_y_OP = 80;
+	
+	// Hand
+	
+	private int position_X_H = 0;
+	private int position_Y_H = 0;
+	private int nbCardbyRow_H = 10;
+	private int nbRow_H = 1;
+	private int scale_width_H = Board.WIDTH_CARD_HAND;
+	private int scale_height_H = Board.HEIGHT_CARD_HAND;
+	private int marge_ext_x_H = 20;
+	private int marge_int_x_H = 10;
+	private int marge_ext_y_H = 10;
+	private int marge_int_y_H = 80;
+	
+	
+	private int width_Screen = 0;
+	private int height_Screen = 0;
+
+	
+	private int widthPanel;
+	private int heightPanel;
+	
+	public GamePanel(int nbPlayer, int widthScreen, int heightScreen) {
+		this.nbPlayer = nbPlayer;
+		this.width_Screen = widthScreen;
+		this.height_Screen = heightScreen;
+		
+		position_Y_H = height_Screen - scale_height_H;
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+		
+		for (int i = 0;i<nbPlayer;i++){
+			flagOtherPlayerBoardCard.add(-1);
+		}
+		
+//		widthPanel = position_X + marge_ext_x*2 + nbCardbyRow*scale_width + marge_int_x*(nbCardbyRow-1);
+//		heightPanel = position_Y + marge_ext_y*2 + nbRow*scale_height + marge_int_y*(nbRow-1);
+//		this.setPreferredSize(new Dimension(widthPanel,heightPanel));
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		updateHand();
+		updatePlayerBoard();
+		updateOtherPlayerBoard();
+		
+		//affiche les cartes du plateau joueur
+		for (Iterator iter = playerBoard.iterator(); iter.hasNext();) {
+			IDrawable d = (IDrawable) iter.next();
+			d.draw(g);	
+		}
+		
+		//affiche les cartes de la main du joueur
+		for (Iterator iter = hand.iterator(); iter.hasNext();) {
+			IDrawable d = (IDrawable) iter.next();
+			d.draw(g);	
+		}
+
+		//affiche les cartes des plateaux des autres joueurs
+		for (Iterator iter = otherPlayerBoard.iterator(); iter.hasNext();) {
+			for(Iterator iter2 = (Iterator) iter.next(); iter.hasNext();){
+				IDrawable d = (IDrawable) iter2.next();
+				d.draw(g);	
+			}
+			
+		}
+		
+		if (zoomCard != null){
+			zoomCard.draw(g);
+		}
+		
+	}
+	
+	
+	// Met à jour toute la main avec une liste de cartes
+	public void updateCards(IDrawableHand hand){
+		this.hand.clear();		
+		for(IDrawableCard card : hand.getCards()){
+			GraphicCard gCard = new GraphicCard(card.getImageId(),card.getImagePath(), 0, 0, scale_width_H, scale_height_H);
+			this.hand.add(gCard);
+			
+		}
+		updateHand();
+		repaint();
+	}
+	
+	
+	
+	// Met à jour toute le plateau du joueur avec une liste de cartes
+	public void updatePlayerBoard(IDrawableBoard board){
+		this.playerBoard.clear();	
+		for(IDrawableCard card : board.getCards()){
+			GraphicCard gCard = new GraphicCard(card.getImageId(),card.getImagePath(), 0, 0, scale_width_P, scale_height_P);
+			this.playerBoard.add(gCard);
+		}
+		updatePlayerBoard();
+		repaint();
+	}
+	
+	// Met à jour toute le plateau du joueur avec une liste de cartes
+	public void updateOtherPlayerBoard(ArrayList<IDrawableBoard> list_board){
+		this.otherPlayerBoard.clear();
+		for (IDrawableBoard board : list_board){
+			ArrayList<GraphicCard> list = new ArrayList<>();
+			
+			for(IDrawableCard card : board.getCards()){
+				GraphicCard gCard = new GraphicCard(card.getImageId(),card.getImagePath(), 0, 0, scale_width_OP, scale_height_OP);
+				list.add(gCard);
+			}
+			this.otherPlayerBoard.add(list);
+		}	
+		updateOtherPlayerBoard();
+		repaint();
+	}
+		
+		
+	
+		
+	// Met à jour l'affichage des cartes de la main joueur(apres une modification)
+	private void updateHand(){
+
+		position_Y_H = height_Screen - scale_height_H - 2*marge_ext_y_H; // Permet de raffraichir la position des cartes en fonction de la taille du JPanel si redimensionnement
+		
+		int x = position_X_H + marge_ext_x_H;
+		int y = position_Y_H + marge_ext_y_H;				
+		for(GraphicCard gc : hand){
+			gc.setX(x);
+			gc.setY(y);
+			if(x == position_X_H + marge_ext_x_H + (scale_width_H + marge_int_x_H)*(nbCardbyRow_H - 1) && y==position_Y_H + marge_ext_y_H){
+				x = position_X_H - scale_width_H - marge_int_x_H + marge_ext_x_H ;
+				y = y + scale_height_H + marge_int_y_H;
+			}
+			x = x + scale_width_H + marge_int_x_H;
+		}
+		repaint();
+	}
+	
+	// Met à jour l'affichage des cartes du plateau du joueur(apres une modification)
+	private void updatePlayerBoard(){
+		int x = position_X_P + marge_ext_x_P;
+		int y = position_Y_P + marge_ext_y_P;				
+		for(GraphicCard gc : playerBoard){
+			gc.setX(x);
+			gc.setY(y);
+			if(x == position_X_P + marge_ext_x_P + (scale_width_P + marge_int_x_P)*(nbCardbyRow_P - 1) && y==position_Y_P + marge_ext_y_P){
+				x = position_X_P - scale_width_P - marge_int_x_P + marge_ext_x_P ;
+				y = y + scale_height_P + marge_int_y_P;
+			}
+			x = x + scale_width_P + marge_int_x_P;
+		}
+		repaint();
+	}
+	
+	private void updateOtherPlayerBoard(){
+		int x = position_X_OP + marge_ext_x_OP;
+		int y = position_Y_OP + marge_ext_y_OP;		
+		
+		for(ArrayList<GraphicCard> list : otherPlayerBoard){
+			for(GraphicCard gc : list){
+				gc.setX(x);
+				gc.setY(y);
+				if(x == position_X_OP + marge_ext_x_OP + (scale_width_OP + marge_int_x_OP)*(nbCardbyRow_OP - 1) && y==position_Y_OP + marge_ext_y_OP){
+					x = position_X_OP - scale_width_OP - marge_int_x_OP + marge_ext_x_OP ;
+					y = y + scale_height_P + marge_int_y_OP;
+				}
+				x = x + scale_width_OP + marge_int_x_OP;
+			}
+		}
+		
+		
+		repaint();
+	}
+	
+	private void updateZoomCard(GraphicCard card){
+		zoomCard = new GraphicCard(card.getImage(),0,0,Board.WIDTH_CARD_ZOOM,Board.HEIGHT_CARD_ZOOM);
+		
+		float ratio = 0.75f;
+		int max_X = width_Screen;
+		int max_Y = height_Screen;
+		
+		int marge_X = 5;
+		int marge_Y = 5;
+		
+		int card_X = card.getX();
+		int card_Y = card.getY();
+		
+		int x = card_X - (Board.WIDTH_CARD_ZOOM - card.getWidth())/2;
+		
+		int y = (int)( card_Y - Board.HEIGHT_CARD_ZOOM + card.getHeight()*ratio);
+		
+		// Par defaut s'affiche a droite du curseur
+		if(x < 0) {
+			x = card_X;
+		} else if (x + Board.WIDTH_CARD_ZOOM > max_X){
+			x = card_X + card.getWidth() - Board.WIDTH_CARD_ZOOM; 
+		} 
+		
+		if(y < 0){
+			y = (int)(card_Y + card.getHeight()*(1-ratio));
+		} 
+		
+		zoomCard.setX(x);
+		zoomCard.setY(y);
+	}
+
+	
+	
+	
+//	// Ajoute un certain nombre de carte à la vue
+//	public void addCards(ArrayList<Card> cards){
+//		for(Card c : cards){
+//			this.playerBoard.add(new GraphicCard(c.getImageId(),c.getImagePath(), 0, 0, scale_width, scale_height));
+//		}
+//		updateGraphicsCards();
+//	}
+//	
+//	// Enleve un certain nombre de carte à la vue
+//	public void removeCards(ArrayList<Integer> index){
+//		ArrayList<GraphicCard> remove = new ArrayList<GraphicCard>();
+//		for(Integer i : index){
+//			remove.add(playerBoard.get(i));
+//		}
+//		playerBoard.removeAll(remove);
+//		updateGraphicsCards();
+//	}
+//		
+	
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// reperage de la carte survolee
+				focusACard = false;
+				
+				for(GraphicCard card : hand){
+					int index = hand.indexOf(card);
+					
+					if(card.getRectangle().contains(e.getPoint())){
+						focusACard = true;
+						if(flagHandCard!=index ){
+							flagHandCard = index;
+							updateZoomCard(card);
+						}
+					}
+				}
+				
+				for(GraphicCard card : playerBoard){
+					int index = playerBoard.indexOf(card);
+					
+					if(card.getRectangle().contains(e.getPoint())){
+						focusACard = true;
+						if(flagPlayerBoardCard!=index ){
+							flagPlayerBoardCard = index;
+							updateZoomCard(card);
+						}
+					}
+				}
+				
+				for(int i = 0 ; i<otherPlayerBoard.size();i++){
+					for(GraphicCard card : otherPlayerBoard.get(i)){
+						int index = otherPlayerBoard.get(i).indexOf(card);
+						
+						if(card.getRectangle().contains(e.getPoint())){
+							focusACard = true;
+							if(flagOtherPlayerBoardCard.get(i)!=index ){
+								flagOtherPlayerBoardCard.set(i,index);
+								updateZoomCard(card);
+							}
+						}
+					}
+				}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				if(!focusACard){
+					flagHandCard = -1;
+					flagPlayerBoardCard = -1;
+					flagOtherPlayerBoardCard.clear();
+					for (int i = 0;i<nbPlayer;i++){
+						flagOtherPlayerBoardCard.add(-1);
+					}
+					zoomCard = null;
+				}
+				
+				
+				
+				
+				
+				repaint();
+				
+	}
+	
+
+	
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	public GraphicCard getFocusedCard(){
+		if(flagHandCard>-1){
+			return hand.get(flagHandCard);
+		} else if(flagPlayerBoardCard>-1){
+			return playerBoard.get(flagPlayerBoardCard);
+		} 
+		
+		for(int i = 0 ; i<flagOtherPlayerBoardCard.size();i++){
+			int index = flagOtherPlayerBoardCard.get(i);
+			if (index>-1){
+				return otherPlayerBoard.get(i).get(index);
+			}
+		}
+		
+		return null;
+	}
+
+	public int getWidthPanel() {
+		return widthPanel;
+	}
+
+	public int getHeightPanel() {
+		return heightPanel;
+	}
+
+	public void setWidth_Screen(int width_Screen) {
+		this.width_Screen = width_Screen;
+	}
+
+	public void setHeight_Screen(int height_Screen) {
+		this.height_Screen = height_Screen;
+	}
+}
